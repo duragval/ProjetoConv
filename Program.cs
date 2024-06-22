551aacddeedb15791db39b92387ff3d9decf9d63
duragval <varleioli@gmail.com>
2024-06-22 15:10:42 -0300

    primeira alteração

--- Program.cs
+++ Program.cs
@@ -1,39 +1,35 @@
-﻿Console.WriteLine("BEM VINDO!!\nDIGITE O VALOR DA CONVERÇÃO\n");
+﻿using System.Drawing;
 
-decimal valor = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("BEM VINDO!!\nDIGITE O VALOR DA CONVERÇÃO\n");
 
-Console.WriteLine("\nDIGITE 1 PARA DÓLAR\nDIGITE 2 PARA EURO\nDIGITE 0 PARA SAIR\n");
-bool end = true;
+decimal valorDigitado = Convert.ToDecimal(Console.ReadLine());
 
-while (end)
+while (true)
 {
+    Console.WriteLine("\nDIGITE 1 PARA DÓLAR\nDIGITE 2 PARA EURO\nDIGITE 3 PARA SAIR\n");
     int convercao = Convert.ToInt32(Console.ReadLine());
 
-    if (convercao == 0)
+    switch (convercao)
     {
-        Console.WriteLine("\nSAINDO DO SISTEMA...");
-        end = false;
+        case 1:
+            valorDigitado = valorDigitado / 5.46m;
+            valorDigitado = Math.Round(valorDigitado, 2);
+            Console.WriteLine($"\nVOCÊ ESCOLHEU DOLAR!\nTOTAL = ${valorDigitado}");
+            break;
+        case 2:
+            valorDigitado = valorDigitado / 5.84m;
+            valorDigitado = Math.Round(valorDigitado, 2);
+            Console.WriteLine($"\nVOCÊ ESCOLHEU EURO!\nTOTAL = {valorDigitado}");
+            break;
+        case 3:
+            Console.WriteLine("\nSAINDO DO SISTEMA...");
+            break;
+        default:
+            Console.WriteLine("\nNÚMERO NÃO RECONHECIDO...\nDIGITE 1 OU 2 PARA ESCOLHER A CONVERÇÃO");
+            Console.WriteLine("OU DIGITE 3 PARA SAIR");
+            break;
     }
-
-    else if (convercao == 1)
-    {
-        valor = valor / 5.46m;
-        valor = Math.Round(valor, 2);
-        Console.WriteLine($"\nVOCÊ ESCOLHEU DOLAR!\nTOTAL = ${valor}");
-        end = false;
-    }
-    else if (convercao == 2)
-    {
-        valor = valor / 5.84m;
-        valor = Math.Round(valor, 2);
-        Console.WriteLine($"\nVOCÊ ESCOLHEU EURO!\nTOTAL = {valor}");
-        end = false;
-    }
-    else
-    {
-        Console.WriteLine("\nNÚMERO NÃO RECONHECIDO...\nDIGITE 1 OU 2 PARA ESCOLHER A CONVERÇÃO");
-        Console.WriteLine("OU DIGITE 0 PARA SAIR");
-    }
+    break;
 }
 
 Console.ReadKey();
